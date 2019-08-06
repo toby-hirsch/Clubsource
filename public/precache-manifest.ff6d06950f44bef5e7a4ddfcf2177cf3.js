b2b271fefb60c1c0f90b09ea0cb79f84ffc4cee6
Toby <toby5252@gmail.com>
2019-08-06 18:48:18 -0400

    finished build

---
+++ public/precache-manifest.ff6d06950f44bef5e7a4ddfcf2177cf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5053fbad0863c34278863d85e297e1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6832ab624bb677c8f4b",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f9f9645c768a93e2ddd5",
+    "url": "/static/css/main.66ef910c.chunk.css"
+  },
+  {
+    "revision": "c6832ab624bb677c8f4b",
+    "url": "/static/js/2.c61b73d2.chunk.js"
+  },
+  {
+    "revision": "f9f9645c768a93e2ddd5",
+    "url": "/static/js/main.08a9db4f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
