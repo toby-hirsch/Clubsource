f36b52a33e1441628042fb3302be041c138c39b1
Toby <toby5252@gmail.com>
2019-08-08 15:42:31 -0400

    added google auth redirect for production

--- googleauth.js
+++ googleauth.js
@@ -1,4 +1,8 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+let baseurl = 'https://greenwich.myclubsource.com';
+
+if (process.env.NODE_ENV=='development')
+	baseurl = 'http://localhost:3000';
 
 module.exports = (passport) => {
     passport.serializeUser((user, done) => {
@@ -10,7 +14,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
             clientID: '118647316730-ra99g2renbeu5dk3qv40iaq1udquh6t5.apps.googleusercontent.com',
             clientSecret: 'uEC5ineqYFDHIN5K07W-97HG',
-            callbackURL: 'http://localhost:3000/auth/google/callback'
+            callbackURL: baseurl + '/auth/google/callback'
         },
         (token, refreshToken, email, done) => {
             return done(null, {
